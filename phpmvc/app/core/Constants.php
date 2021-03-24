ef782fe35e27bb9cb2226d8c433f386e925764c4
unknown <susanto@infimedia.co.id>
2021-03-24 19:37:27 +0700

    add Praktek Pertemuan 7,8,9

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php 
+    define('BASEURL','http://localhost/Pemrograman%20Web/phpmvc/public');
+?>
---
+++ phpmvc/app/init.php
@@ -0,0 +1,5 @@
+<?php
+    require_once 'core/App.php'; //Sebagai class App
+    require_once 'core/Controller.php'; //sebagai class Controller
+    require_once 'core/Constants.php';
+?>
